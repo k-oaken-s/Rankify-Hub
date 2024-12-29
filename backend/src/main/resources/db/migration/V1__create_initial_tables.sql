8b8655e349ddbc7384d1849b4043a12e467d93ed
Koji Suehiro <tiripeppa@gmail.com>
2024-12-29 22:59:58 +0900

    マイグレーションファイルを結合してinitial schema作成

--- backend/src/main/resources/db/migration/V1__create_initial_tables.sql
+++ backend/src/main/resources/db/migration/V1__create_initial_tables.sql
@@ -1,29 +1,57 @@
-CREATE TABLE category
-(
-    id          UUID PRIMARY KEY,
-    name        VARCHAR(255) NOT NULL,
-    description TEXT,
-    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE category (
+                          id          UUID PRIMARY KEY,
+                          name        VARCHAR(255) NOT NULL,
+                          description TEXT,
+                          image       VARCHAR(512), -- S3保存先URLを格納
+                          created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                          updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE item
-(
-    id          UUID PRIMARY KEY,
-    category_id UUID         NOT NULL,
-    name        VARCHAR(255) NOT NULL,
-    image_url   VARCHAR(255),
-    description TEXT,
-    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE CASCADE
+CREATE TABLE item (
+                      id          UUID PRIMARY KEY,
+                      category_id UUID         NOT NULL,
+                      name        VARCHAR(255) NOT NULL,
+                      image       VARCHAR(512), -- S3保存先URLを格納
+                      description TEXT,
+                      created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                      updated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                      FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE CASCADE
 );
 
-CREATE TABLE administrator
-(
-    id            BIGSERIAL PRIMARY KEY,
-    username      VARCHAR(255) NOT NULL UNIQUE,
-    password_hash VARCHAR(255) NOT NULL,
-    created_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE user_tier (
+                           id           UUID PRIMARY KEY,
+                           anonymous_id VARCHAR(255) NOT NULL,
+                           category_id  UUID         NOT NULL,
+                           name         VARCHAR(255) NOT NULL,
+                           is_public    BOOLEAN      NOT NULL DEFAULT FALSE,
+                           access_url   VARCHAR(255) NOT NULL,
+                           created_at   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                           updated_at   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                           CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE CASCADE
+);
+
+CREATE TABLE user_tier_level (
+                                 id           UUID PRIMARY KEY,
+                                 user_tier_id UUID         NOT NULL,
+                                 name         VARCHAR(255) NOT NULL,
+                                 order_index  INT          NOT NULL,
+                                 created_at   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                 updated_at   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                 CONSTRAINT fk_user_tier FOREIGN KEY (user_tier_id) REFERENCES user_tier (id) ON DELETE CASCADE,
+                                 UNIQUE (user_tier_id, name),
+                                 UNIQUE (user_tier_id, order_index)
+);
+
+CREATE TABLE user_tier_level_item (
+                                      id                 UUID PRIMARY KEY,
+                                      user_tier_id       UUID      NOT NULL,
+                                      user_tier_level_id UUID      NOT NULL,
+                                      item_id            UUID      NOT NULL,
+                                      order_index        INT       NOT NULL,
+                                      created_at         TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                      updated_at         TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                      CONSTRAINT fk_user_tier_item FOREIGN KEY (user_tier_id) REFERENCES user_tier (id) ON DELETE CASCADE,
+                                      CONSTRAINT fk_user_tier_level_item FOREIGN KEY (user_tier_level_id) REFERENCES user_tier_level (id) ON DELETE CASCADE,
+                                      CONSTRAINT fk_item FOREIGN KEY (item_id) REFERENCES item (id) ON DELETE CASCADE,
+                                      UNIQUE (user_tier_level_id, order_index)
 );
--- backend/src/main/resources/db/migration/V3__add_image_column_for_category.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE category
-    ADD COLUMN image BYTEA;
